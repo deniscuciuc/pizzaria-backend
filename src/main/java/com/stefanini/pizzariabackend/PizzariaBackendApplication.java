01b3cb722344857738a74130402cdf13673fafd1
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-08-27 23:38:40 +0300

    Init project

---
+++ src/main/java/com/stefanini/pizzariabackend/PizzariaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stefanini.pizzariabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefanini/pizzariabackend/PizzariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanini.pizzariabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
