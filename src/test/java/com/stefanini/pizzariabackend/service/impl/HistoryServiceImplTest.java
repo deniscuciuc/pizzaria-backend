66f1540d30828f9a5666d9aacb2277547b5c2160
Denis Cuciuc <dev.dcuciuc@gmail.com>
2022-10-03 05:31:20 +0300

    Created test

---
+++ src/test/java/com/stefanini/pizzariabackend/service/impl/HistoryServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.stefanini.pizzariabackend.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HistoryServiceImplTest {
+
+
+
+    @Test
+    void saveHistory() {
+    }
+
+    @Test
+    void findAllHistories() {
+    }
+
+    @Test
+    void deleteHistoryById() {
+    }
+}
